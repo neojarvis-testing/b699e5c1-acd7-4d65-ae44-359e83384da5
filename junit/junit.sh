512efe45634964f8e9179833bdf039aaaeef1080
Teacher-Venkatesh <12venkat21@gmail.com>
2021-09-18 14:38:18 +0000

    First Commit

---
+++ junit/junit.sh
@@ -0,0 +1,3 @@
+cp -r /home/coder/project/workspace/junit/test /home/coder/project/workspace/BookStore-Application-Angular/spring/src/;
+cd /home/coder/project/workspace/BookStore-Application-Angular/spring/;
+mvn clean test
